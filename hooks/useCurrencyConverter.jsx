52c08dfaef6666be80b61ae70c4a9644d55425fe
neemaseruhar <neemaserugar@gmail.com>
2024-01-09 16:34:37 +0530

    initial commit

---
+++ hooks/useCurrencyConverter.jsx
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react'
+import { useState } from 'react'
+
+export const useCurrencyConverter = (currency) => {
+
+  const [data,setData] = useState({});
+
+  useEffect(() =>{
+
+    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+      .then(res => res.json())
+      .then(json => setData(json[currency]))
+
+  },[currency])
+ 
+
+  return data;
+}
